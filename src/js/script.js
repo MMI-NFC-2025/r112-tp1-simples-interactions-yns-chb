ab95ce91943fc3c885648d0ab2d942e616a233ae
Yanis Chuhib-Forster <yanis.chouhib-forster@edu.univ-fcomte.fr>
2025-11-07 11:15:56 +0100

    les titres H2 deviennent rouge suite à un click

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,12 @@
 const aMettreEnRouge= document.querySelector ('#a-mettre-en-rouge');
 aMettreEnRouge.style.color = "red"
 
+
+
+const collH2 = document.querySelectorAll("H2")
+collH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = 'red';
+    });
+});
+    
